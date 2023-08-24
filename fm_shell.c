7a168aee659b714b5d4fce1c42e5059a3b64f35d
FearGod Okeke <okekefeargod@gmail.com>
2023-08-24 04:13:06 -0700

    factored in the non-interactive mode

--- fm_shell.c
+++ fm_shell.c
@@ -1,13 +1,55 @@
 #include "fm_main.h"
 
 /**
-* main - a simple shell.
-* @ac: argument count.
-* @av: argument vector.
-*
-* Return: Always 0.
-*/
-int main(int ac __attribute__((unused)), char **av)
+ * batch_mode - to handle batch mode.
+ * @av: argument vector.
+ *
+ * Return: Nothing.
+ */
+void batch_mode(char **av)
+{
+	char *lineptr, *lineptr_cpy;
+	const char *delimtr = " \n";
+	size_t nread = 0;
+	int count_token = 0;
+	ssize_t nchar_read;
+	char **av_cmd_line;
+	pid_t child_pid;
+
+	while ((nchar_read = getline(&lineptr, &nread, stdin)) != -1)
+	{
+		fm_builtin(lineptr);
+		lineptr_cpy = malloc(nchar_read - 1);
+		mem_alloc_error(lineptr_cpy);
+		strncpy(lineptr_cpy, lineptr, nchar_read - 1);
+		count_token = fm_tokenize(lineptr, delimtr);
+		av_cmd_line = av_cmmd(lineptr_cpy, delimtr, av, count_token);
+		child_pid = fork();
+		if (child_pid == 0)
+		{
+			exe_cmmd(av_cmd_line);
+			exit(0);
+		}
+		else if (child_pid < 0)
+		{
+			perror("Fork failed");
+		}
+		else
+		{
+			wait(NULL);
+		}
+			free(lineptr_cpy);
+	}
+	free(lineptr);
+	exit(0);
+}
+/**
+ * interactive_mode - to handle interactive mode.
+ * @av: argument vector.
+ *
+ * Return: 0 on suchess, -1 on failure.
+ */
+int interactive_mode(char **av)
 {
 	char *lineptr, *lineptr_cpy, *shell_prompt = "fm_shell$ ";
 	const char *delimtr = " \n";
@@ -19,14 +61,12 @@ int main(int ac __attribute__((unused)), char **av)
 
 	for (;;)
 	{
-		write(STDOUT_FILENO, shell_prompt, _strlen(shell_prompt));
+		write(STDOUT_FILENO, shell_prompt, strlen(shell_prompt));
 		nchar_read = getline(&lineptr, &nread, stdin);
 		fm_builtin(lineptr);
 		if (nchar_read == -1)
-		{
-			write(STDOUT_FILENO, "Exiting...\n", 11);
 			return (-1);
-		}
+
 		else
 		{
 			lineptr_cpy = malloc(sizeof(char) * nchar_read);
@@ -48,5 +88,23 @@ int main(int ac __attribute__((unused)), char **av)
 	}
 	free(lineptr);
 	free(lineptr_cpy);
+
+	return (0);
+}
+/**
+* main - a simple shell.
+* @ac: argument count.
+* @av: argument vector.
+*
+* Return: Always 0.
+*/
+int main(int ac __attribute__((unused)), char **av)
+{
+	if (!isatty(fileno(stdin)))
+		batch_mode(av);
+
+	else
+		interactive_mode(av);
+
 	return (0);
 }
